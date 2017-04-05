51a9c333744a22842acbb5e3a3971f43838b95df
Glynnis Thomas <glynnisthomas@gmail.com>
2017-04-04 19:59:40 -0500

    Completed TODO #7 and removed remaining TODO notations. Tested everything, seems to be working just fine.

--- src/TechJobs/ViewModels/BaseViewModel.cs
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
     public class BaseViewModel
     {
+        // All columns, for display
+        public List<JobFieldType> Columns { get; set; }
 
+        // View title
+        public string Title { get; set; } = "";
+
+        // Populate the list of all columns
+        public List<JobFieldType> PopulateColumns()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+
+            return Columns;
+        }
     }
 }
